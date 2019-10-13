23f5bfa9573d99f5115f5728c965106967569eb8
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2019-10-13 13:59:50 -0300

    cuidados com Number

---
+++ exercicios-web/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,15 @@
+//tipo infinito 
+console.log(7 / 0)
+
+//converte a string automaticamente "5"
+console.log("10" / 2)
+
+console.log("Show" * 2)
+
+//0.7999999...
+console.log(0.1 + 0.7)
+
+console.log((10).toFixed(2))
+
+
+
