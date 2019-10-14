d2494dca6889b51bdbbb84b721a01e65c759bb54
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2019-10-14 16:09:29 -0300

    basico de objetos em JavaScript

---
+++ exercicios-web/fundamentos/objetos.js
@@ -0,0 +1,15 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 5000
+prod1['descontoLegal'] = 0.4
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.99,
+    tamanho: 'M'
+}
+
+console.log(prod2)
+
