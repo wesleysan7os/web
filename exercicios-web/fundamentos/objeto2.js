2008d12b7f889225ea22e035911c7d73c8dc5e22
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2019-10-16 23:22:14 -0300

    instanciando funçoes

---
+++ exercicios-web/fundamentos/objeto2.js
@@ -0,0 +1,10 @@
+console.log(typeof Object) //function
+console.log(typeof new Object) //object
+
+const Cliente = function() {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+class Produto {} // ES 2015 (ES6)
+console.log(typeof Produto)
+console.log(typeof new Produto())
