7a4aba5cb959a0c715e79a1934ecd2d424603ee1
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2019-11-05 21:27:59 -0300

    estrutura de controle if

---
+++ exercicios-web/controle/if2.js
@@ -0,0 +1,18 @@
+function teste(num) {
+    if(num > 7) 
+        console.log(num)
+        console.log('final')
+    
+}
+
+//teste(6)
+//teste(8)
+
+function teste2(num) {
+    if(num > 7); {
+        console.log(num)
+    } 
+}
+
+teste2(6)
+teste2(8)
