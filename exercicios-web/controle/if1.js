87a0daef0c7d5698e119c501f4e1081e93bc9af4
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2019-11-05 21:20:08 -0300

    estrutura de controle if

---
+++ exercicios-web/controle/if1.js
@@ -0,0 +1,28 @@
+function soBoaNoticia(nota) {
+    if(nota >= 7) {
+        console.log('Aprovado com ' + nota)
+    }
+}
+
+soBoaNoticia(8.1)
+soBoaNoticia(5.0)
+
+function seForVerdadeEuFalo(valor) {
+    if(valor) {
+        console.log('É verdade... ' + valor)
+    }
+}
+
+/* false */
+seForVerdadeEuFalo()
+seForVerdadeEuFalo(null)
+seForVerdadeEuFalo(undefined)
+seForVerdadeEuFalo(NaN)
+seForVerdadeEuFalo('')
+seForVerdadeEuFalo(0)
+
+/* true */
+seForVerdadeEuFalo(-1)
+seForVerdadeEuFalo(' ')
+seForVerdadeEuFalo([])
+seForVerdadeEuFalo({})
