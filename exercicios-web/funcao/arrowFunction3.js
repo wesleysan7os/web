1d2ec0e5d026dc155dd6e8b87dd875acf9bb34a6
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2020-08-22 13:05:45 -0300

    exercicios sobre funções anônimas

--- exercicios-web/funcao/arrowFunction3.js
+++ exercicios-web/funcao/arrowFunction3.js
@@ -11,3 +11,8 @@ comparaComThis(obj);
 
 let comparaComThisArrow = param => console.log(this === param);
 comparaComThisArrow(global);
+comparaComThisArrow(module.exports);
+
+comparaComThisArrow = comparaComThisArrow.bind(obj);
+comparaComThisArrow(obj);
+comparaComThisArrow(module.exports);
