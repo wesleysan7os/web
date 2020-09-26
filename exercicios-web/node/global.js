79a8bad5c9fb0d1144e07396a28d129ef2af3b02
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2020-09-25 21:55:08 -0300

    definindo objetos no escopo global e usando o método freeze de Object

---
+++ exercicios-web/node/global.js
@@ -0,0 +1,8 @@
+//console.log(global)
+
+global.MinhaApp = Object.freeze({
+    saudacao() {
+        return 'Estou aqui!'
+    },
+    nome: 'Sistema Legal'
+})
---
+++ exercicios-web/node/globalCliente.js
@@ -0,0 +1,6 @@
+require('./global')
+
+console.log(MinhaApp.saudacao())
+
+MinhaApp.nome = 'Opa!'
+console.log(MinhaApp.nome)
