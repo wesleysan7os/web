b21693c9f7fcda8dac00c59237bf5ed52024f2d9
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2020-09-28 23:24:59 -0300

    API com Express

--- exercicios-web/node/projeto/src/servidor.js
+++ exercicios-web/node/projeto/src/servidor.js
@@ -2,11 +2,24 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 3000.00}) // Converte para JSON
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta, () => {
-    console.log(`Servidor executando na porta ${porta}.`)
+    console.log(`Servidor executando na porta ${porta}.`) //JSON
 })
