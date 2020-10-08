6049d823b159cdcedb95e754e47aaf757db9762c
wesleysan7os <wesley.santos.silva@ccc.ufcg.edu.br>
2020-10-08 19:24:56 -0300

    lendo arquivo com promises

---
+++ exercicios-web/esnext/desafioPromise.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const path = require('path')
+
+
+function lerArquivo(caminho) {
+    return new Promise(function(resolve) {
+        fs.readFile(caminho, function(_, conteudo) {
+            resolve(conteudo.toString())
+        })
+    })
+}
+
+const caminho = path.join(__dirname, 'dados.txt')
+lerArquivo(caminho).then(console.log)
